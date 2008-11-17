073d883fc4c6e69086057b2b1fc8c30dc19457aa
Edward Vielmetti <edward.vielmetti@gmail.com>
2008-11-17 15:26:12 -0500

    first commit

---
+++ month-num-to-name.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sed \
+   -e 's/-01-/-January-/' \
+   -e 's/-02-/-February-/' \
+   -e 's/-03-/-March-/' \
+   -e 's/-04-/-April-/' \
+   -e 's/-05-/-May-/' \
+   -e 's/-06-/-June-/' \
+   -e 's/-07-/-July-/' \
+   -e 's/-08-/-August-/' \
+   -e 's/-09-/-September-/' \
+   -e 's/-10-/-October-/' \
+   -e 's/-11-/-November-/' \
+   -e 's/-12-/-December-/'
