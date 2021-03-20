c541db74bc891b0c8be965225a121f3d573eb393
Edward Vielmetti <ed@packet.net>
2021-03-20 18:53:45 -0400

    get some more joins in here to give me more insight

--- files/lib.steampipe/devices.sql
+++ files/lib.steampipe/devices.sql
@@ -1,9 +1,17 @@
-select 
-  hostname,
-  id,
-  created_at,
-  project_id
-from 
-  equinix_metal_device 
-order by 
-  created_at
+select
+  d.hostname,
+  d.id,
+  d.created_at,
+  d.project_id,
+  d.state,
+  p.name,
+  plan.name as plan
+from
+  equinix_metal_device as d,
+  equinix_metal_project as p,
+  equinix_metal_plan as plan
+where
+  d.project_id = p.id
+  and plan.id = d.plan_id
+order by
+  d.created_at
