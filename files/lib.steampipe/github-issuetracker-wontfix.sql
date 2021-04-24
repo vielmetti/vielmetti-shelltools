b125b63f2d2badaafeba50dcdda028f1cc49b222
Edward Vielmetti <edward.vielmetti@gmail.com>
2021-04-24 12:36:59 -0400

    steampipe: github examples
    Signed-off-by: Edward Vielmetti <edward.vielmetti@gmail.com>

---
+++ files/lib.steampipe/github-issuetracker-wontfix.sql
@@ -0,0 +1,12 @@
+select
+  repository_full_name,
+  issue_number,
+  title,
+  state,
+  tags
+from
+  github_issue
+where
+  repository_full_name = 'vielmetti/issuetracker'
+  and state = 'open'
+  and tags ? 'wontfix';
